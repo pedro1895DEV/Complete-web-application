22314a63c342d336331335d6f395ff20def675d8
pedro1895DEV <95310737+pedro1895DEV@users.noreply.github.com>
2023-01-26 15:27:29 -0300

    user e delete

--- js/index.js
+++ js/index.js
@@ -18,6 +18,7 @@ const client = new pg.Client({
 
 client.connect()
 
+//Verificar pendências de um usuário
 app.get("/usuario/:id", (req, res) => {
     client.query({
         text: `SELECT nome_usuario, id_usuario, tipo_ocorrencia FROM usuario, ocorrencia WHERE ocorrencia.id_ocorrencia = $1`,
@@ -29,8 +30,51 @@ app.get("/usuario/:id", (req, res) => {
                 resultado: ret.rows[0]
             })
         })
-})
+});
+
+//Criar um usuário
+app.post('/usuario', function (req, res) {
+    client.query(
+        {
+            text:
+                'INSERT INTO usuario (nome_usuario, idade, cpf, email) VALUES ($1, $2, $3, $4)',
+            values:
+                [req.body.nome_usuario, req.body.idade,
+                req.body.cpf, req.body.email]
+        }
+    )
+        .then(
+            function (ret) {
+                res.json(
+                    {
+                        dadosEnviados: req.body
+                    }
+                )
+            }
+        );
+});
+
+//Deletar um usuário existente
+app.post('/usuarioo', function (req, res) {
+    client.query(
+        {
+            text:
+                'DELETE FROM usuario WHERE id_usuario = $1',
+            values:
+                [req.body.id_usuario]
+        }
+    )
+        .then(
+            function (ret) {
+                res.json(
+                    {
+                        dadosEnviados: req.body
+                    }
+                )
+            }
+        );
+});
 
 app.listen(
     3000
-)
+);
--- js/script.js
+++ js/script.js
@@ -1,11 +1,60 @@
+//Verificar pendências de um usuário
 $("#botao").click(() => {
     let numero = $('#id-usuario').val();
     $.ajax({
-        type :'GET',
-        url : `http://localhost:3000/usuario/${numero}`,
-        success : function (res){
+        type: 'GET',
+        url: `http://localhost:3000/usuario/${numero}`,
+        success: function (res) {
             $('#nome').html(res.resultado.nome_usuario)
-            $('#ocorrencia').html(res.resultado.tipo_ocorrencia)              
+            $('#ocorrencia').html(res.resultado.tipo_ocorrencia)
         }
     })
-})
+});
+
+//Criar um usuário
+$('#botao2').click(
+    () => {
+        let nome1 = $('#nome').val();
+        let idade1 = $('#idade').val();
+        let cpf1 = $('#cpf').val();
+        let email1 = $('#email').val();
+        $.ajax(
+            {
+                type: 'POST',
+                url: `http://localhost:3000/usuario/`,
+                data: JSON.stringify(
+                    {
+                        nome_usuario: nome1,
+                        idade: idade1,
+                        cpf: cpf1,
+                        email: email1
+                    }
+                ),
+                contentType: "application/json",
+                dataType: 'json'
+            }
+        )
+    }
+);
+
+//Deletar um usuário existente
+
+$('#botao3').click(
+    () => {
+        let numero2 = $('#digitado').val();
+
+        $.ajax(
+            {
+                type: 'POST',
+                url: `http://localhost:3000/usuarioo/`,
+                data: JSON.stringify(
+                    {
+                        id_usuario: numero2
+                    }
+                ),
+                contentType: 'application/json'
+
+            }
+        )
+    }
+)
